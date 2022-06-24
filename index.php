2160edb32c0751aa90305825d679628f53b0cf40
603-kousei ninomiya <75236360+kousei-py-tech@users.noreply.github.com>
2022-06-24 12:01:05 +0900

    ちょっこと変更

--- index.php
+++ index.php
@@ -10,10 +10,10 @@ session_start();
 <body>
 <header>
     <div>
-        <a href="/vantan_board/index.php">TOP</a>
-        <a href="/vantan_board/register.php">新規作成</a>
-        <a href="/vantan_board/login.php">ログイン</a>
-        <a href="/vantan_board/logout.php">ログアウト</a>
+        <a href="vantan_board/index.php">TOP</a>
+        <a href="vantan_board/register.php">新規作成</a>
+        <a href="vantan_board/login.php">ログイン</a>
+        <a href="vantan_board/logout.php">ログアウト</a>
     </div>
     <h1>トップ</h1>
 </header>
