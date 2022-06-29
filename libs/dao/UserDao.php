8e634cb199df0d326af49004126f376967f5f1da
603-kousei ninomiya <75236360+kousei-py-tech@users.noreply.github.com>
2022-06-29 14:07:33 +0900

    オブジェクト指向

---
+++ libs/dao/UserDao.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once __DIR__ . '/Database.php';
+require_once __DIR__ . '/../entity/BoardEntity.php';
+
+class UserDao extends Database
+{
+    public function  insert($mail, $password, $name)
+  {
+    $sql = 'INSERT INTO `users` (name, mail, password, created, modified)';
+    $sql .= ' VALUES (:name, :mail, :password, NOW(), NOW())';
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->bindValue(':name', $name, \PDO::PARAM_STR);
+    $stmt->bindValue(':mail', $mail, \PDO::PARAM_STR);
+    $stmt->bindValue(':password', $password, \PDO::PARAM_STR);
+    $result = $stmt->execute();
+    if ($result) {
+      return $this->pdo->lastInsertId();
+  } else {
+      return false;
+  }
+  }
+} 
